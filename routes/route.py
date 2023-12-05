d22b8a35183c61dc1a44128eba6a4960f7f4144d
hoyirul <irulgraspama621@gmail.com>
2023-12-05 23:30:47 +0700

    Fix route

--- routes/route.py
+++ routes/route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, redirect, session
 from controllers.user_controller import UserController
 from controllers.about_controller import AboutController
 from controllers.welcome_controller import WelcomeController
@@ -10,15 +10,22 @@ aboutController = AboutController()
 welcomeController = WelcomeController()
 authController = AuthController()
 
-@main.route('/')
+@main.route('/', methods=['GET'])
 def index():
+    # if 'user_id' in session:
+    return welcomeController.index()
+    # else:
+        # return redirect('/login')
+
+@main.route('/login', methods=['GET'])
+def login():
     return authController.index()
 
 @main.route('/auth/login', methods=['POST'])
 def auth_login():
     return authController.auth_login()
 
-@main.route('/user/<int:user_id>')
+@main.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     return userController.get_user(user_id)
 
@@ -27,7 +34,7 @@ def update_user(user_id):
     # Ambil data yang diperlukan dari request dan panggil fungsi update pada user_controller
     pass
 
-@main.route('/about/<int:id>')
+@main.route('/about/<int:id>', methods=['GET'])
 def get_about(id):
     return aboutController.get_about(id)
 
