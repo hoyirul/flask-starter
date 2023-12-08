a583fbfeca6e744ca8c29311b0042c846c965c83
hoyirul <mochammad.hairullah@gmail.com>
2023-12-08 09:36:49 +0700

    Fix error page and route

--- routes/route.py
+++ routes/route.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, request, redirect, session
-from controllers.user_controller import UserController
-from controllers.about_controller import AboutController
 from controllers.welcome_controller import WelcomeController
 from controllers.auth_controller import AuthController
 from controllers.compressor_af_controller import CompressorAfController
@@ -8,8 +6,6 @@ from controllers.theme_anomaly_controller import ThemeAnomalyController
 from middlewares.middleware import AuthMiddleware
 
 main = Blueprint('main', __name__)
-userController = UserController()
-aboutController = AboutController()
 welcomeController = WelcomeController()
 authController = AuthController()
 compressorAfController = CompressorAfController()
@@ -43,20 +39,3 @@ def compressor_af():
 @authMiddleware.authorized
 def theme_anomalies():
     return themeAnomalyController.index()
-
-@main.route('/user/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    return userController.get_user(user_id)
-
-@main.route('/user/<int:user_id>/update', methods=['POST'])
-def update_user(user_id):
-    # Ambil data yang diperlukan dari request dan panggil fungsi update pada user_controller
-    pass
-
-@main.route('/about/<int:id>', methods=['GET'])
-def get_about(id):
-    return aboutController.get_about(id)
-
-@main.route('/store', methods=['POST'])
-def store():
-    return userController.store()
--- utils/handler_error.py
+++ utils/handler_error.py
@@ -2,13 +2,33 @@ from flask import render_template
 
 class HandlerError:
     def not_found_error(self, error):
-        return render_template('errors/404.html'), 404
+        e = {
+            'code': 404,
+            'message': 'Not Found',
+            'description': 'The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'
+        }
+        return render_template('errors/error.html', e=e), 404
 
     def internal_error(self, error):
-        return render_template('errors/500.html'), 500
+        e = {
+            'code': 500,
+            'message': 'Internal Server Error',
+            'description': 'The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.'
+        }
+        return render_template('errors/error.html', e=e), 500
 
     def method_not_allowed(self, error):
-        return render_template('errors/405.html'), 405
+        e = {
+            'code': 405,
+            'message': 'Method Not Allowed',
+            'description': 'The method is not allowed for the requested URL.'
+        }
+        return render_template('errors/error.html', e=e), 405
 
     def bad_request(self, error):
-        return render_template('errors/400.html'), 400
+        e = {
+            'code': 400,
+            'message': 'Bad Request',
+            'description': 'The browser (or proxy) sent a request that this server could not understand.'
+        }
+        return render_template('errors/error.html', e=e), 400
